5f4fef12bc4385ee9c3019142dde8195c94d65c1
CoderBoy25 <88992596+CoderBoy25@users.noreply.github.com>
2022-09-27 05:10:17 -0500

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,14 @@
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA78dV3PR7NFBkMQECKg-QRrbRsXsplVGI",
+  authDomain: "kwitter-webpage-97d7a.firebaseapp.com",
+  databaseURL: "https://kwitter-webpage-97d7a-default-rtdb.firebaseio.com",
+  projectId: "kwitter-webpage-97d7a",
+  storageBucket: "kwitter-webpage-97d7a.appspot.com",
+  messagingSenderId: "1030339291421",
+  appId: "1:1030339291421:web:0cc99926b273f7ad54d9f4",
+  measurementId: "G-MWSSLJKMGF"
+};
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
