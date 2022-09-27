a905b632ba552d3369d37c65c62d8a97eded7e69
CoderBoy25 <88992596+CoderBoy25@users.noreply.github.com>
2022-09-27 05:07:30 -0500

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -4,7 +4,6 @@ user_name = document.getElementById("user_name").value;
 localStorage.setItem("user_name",user_name);
 window.location = ("kwitter_room.html");
 }
-
 function addRoom()
 {
     room_name = document.getElementById("room_name").value;
