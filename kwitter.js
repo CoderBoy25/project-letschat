4f3ad585113f0b3c461905409f45222974001600
CoderBoy25 <88992596+CoderBoy25@users.noreply.github.com>
2022-10-10 09:50:34 -0500

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,20 +1,10 @@
-function addUser()
-{
-user_name = document.getElementById("user_name").value;
-localStorage.setItem("user_name",user_name);
-window.location = ("kwitter_room.html");
-}
-function addRoom()
-{
-    room_name = document.getElementById("room_name").value;
 
+function addUser() {
 
-    firebase.database().ref("/").child(room_name).update({
-        purpose : "adding room name"
-    });
+  user_name = document.getElementById("user_name").value;
 
-    localStorage.setItem("room_name", room_name);
-
-
+  localStorage.setItem("user_name", user_name);
+  
     window.location = "kwitter_room.html";
 }
+
